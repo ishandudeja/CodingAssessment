94c526d76642c421f72acd6ad3a670dac98768ab
ishandudeja <ishandudeja@gmail.com>
2020-11-06 09:46:52 +1300

    defining game controller and user interaction code

--- CleanGreenFieldsProject/Program.cs
+++ CleanGreenFieldsProject/Program.cs
@@ -10,7 +10,26 @@ namespace CleanGreenFieldsProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PlayingCard playingCard = new PlayingCard();
+            List<Card> deck = playingCard.CreateDeck();
+            playingCard.Shuffle();
+            List<Card> myCards = playingCard.GetYou3Cards();
+            Console.WriteLine("Welcome to Card Game");
+            bool result = true;
+
+            do
+            {
+
+                Console.WriteLine("You have these cards");
+                playingCard.ShowCards(myCards);
+                Console.WriteLine("To Show you cards, Please enter card index");
+                int cardIndex = Convert.ToInt32(Console.ReadLine());
+                if (cardIndex <= myCards.Count && cardIndex > 0)
+                {
+                    result = playingCard.GetWinResult(myCards[cardIndex]);
+                    myCards.RemoveAt(cardIndex - 1);
+                }
+            } while (result);
             Console.ReadKey();
         }
     }
