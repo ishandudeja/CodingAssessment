2171aa4d05e99ee0e4ee64ad6baeeed0f0a2758a
ishandudeja <ishandudeja@gmail.com>
2020-11-06 09:32:56 +1300

    Adding Create Deck Functon

---
+++ CleanGreenFieldsProject/PlayingCard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanGreenFieldsProject
+{
+  public class PlayingCard
+    {
+        private List<Card> _Deck;
+        public PlayingCard()
+        {
+            _Deck = new List<Card>();
+        }
+        public List<Card> CreateDeck()
+        {
+            foreach (CardFace cardFace in Enum.GetValues(typeof(CardFace)))
+            {
+                foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+                {
+                    _Deck.Add(new Card(cardFace, suit));
+                }
+            }
+
+            return _Deck;
+        }
+    }
+}
